55e9e15158e0141c17fea996b4f4c6e917401082
Alvaro Rodriguez Gonzalez <alvamige@gmail.com>
2025-07-26 09:37:01 +0200

    Add carousels-service ECR module

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
-module "spring-boot-template" {
+module "carousels-service" {
   source = "./modules/ecr"
 
-  name = "spring-boot-template"
-}
-
-module "nodejs-template" {
-  source = "./modules/ecr"
-
-  name = "nodejs-template"
+  name = "carousels-service"
 }
